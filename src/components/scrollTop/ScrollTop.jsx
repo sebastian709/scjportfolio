58b83147f67c6f69b62ee72bcc2d6b20cbaf5f6a
ASL <sebastianjabson07@gmail.com>
2026-01-20 09:11:34 +0800

    Update ScrollTop.jsx

--- src/components/scrollTop/ScrollTop.jsx
+++ src/components/scrollTop/ScrollTop.jsx
@@ -4,20 +4,15 @@ import "./ScrollTop.css";
 const ScrollTop = () => {
   const [visible, setVisible] = useState(false);
 
-  // Show button after scrolling down 300px
   useEffect(() => {
     const toggleVisibility = () => {
-      if (window.scrollY > 300) {
-        setVisible(true);
-      } else {
-        setVisible(false);
-      }
+      if (window.scrollY > 300) setVisible(true);
+      else setVisible(false);
     };
     window.addEventListener("scroll", toggleVisibility);
     return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
 
-  // Scroll smoothly to top
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -29,7 +24,7 @@ const ScrollTop = () => {
     <>
       {visible && (
         <button className="scroll-top" onClick={scrollToTop}>
-          <i class="ri-arrow-up-s-line"></i>
+          <i className="ri-arrow-up-s-line"></i>
         </button>
       )}
     </>
